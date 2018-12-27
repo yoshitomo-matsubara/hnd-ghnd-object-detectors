de3313c71966cb5388ed946b19e09cedbcf559e4
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2018-12-26 20:28:07 -0800

    aggressively refactored

--- src/utils/eval_util.py
+++ src/utils/eval_util.py
@@ -5,6 +5,19 @@ import torch
 from pycocotools.cocoeval import COCOeval
 
 
+def calc_iou(a, b):
+    area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+    iw = torch.min(torch.unsqueeze(a[:, 2], dim=1), b[:, 2]) - torch.max(torch.unsqueeze(a[:, 0], 1), b[:, 0])
+    ih = torch.min(torch.unsqueeze(a[:, 3], dim=1), b[:, 3]) - torch.max(torch.unsqueeze(a[:, 1], 1), b[:, 1])
+    iw = torch.clamp(iw, min=0)
+    ih = torch.clamp(ih, min=0)
+    ua = torch.unsqueeze((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), dim=1) + area - iw * ih
+    ua = torch.clamp(ua, min=1e-8)
+    intersection = iw * ih
+    iou = intersection / ua
+    return iou
+
+
 def evaluate_coco(dataset, model, threshold=0.05):
     model.eval()
     with torch.no_grad():
