475e4c8c09d915e8c19ee6d770599deacb09e007
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-01-14 14:57:40 -0800

    refactored

--- src/utils/retinanet_util.py
+++ src/utils/retinanet_util.py
@@ -16,21 +16,21 @@ def get_datasets(dataset_config):
         train_dataset = CocoDataset(train_data_config['annotation'], train_data_config['img_dir'],
                                     transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]))
         val_data_config = data_config['val']
-        valid_dataset = CocoDataset(val_data_config['annotation'], val_data_config['img_dir'],
-                                    transform=transforms.Compose([Normalizer(), Resizer()]))
-        return train_dataset, valid_dataset
+        val_dataset = CocoDataset(val_data_config['annotation'], val_data_config['img_dir'],
+                                  transform=transforms.Compose([Normalizer(), Resizer()]))
+        return train_dataset, val_dataset
     elif dataset_name == 'csv':
         class_file_path = data_config['class']
         train_dataset = CSVDataset(data_file_path=data_config['train'], class_list=class_file_path,
                                    transform=transforms.Compose([Normalizer(), Augmenter(), Resizer()]))
         val_file_path = data_config['val']
         if not file_util.check_if_exists(val_file_path):
-            valid_dataset = None
+            val_dataset = None
             print('No validation annotations provided.')
         else:
-            valid_dataset = CSVDataset(data_file_path=val_file_path, class_list=class_file_path,
-                                       transform=transforms.Compose([Normalizer(), Resizer()]))
-        return train_dataset, valid_dataset
+            val_dataset = CSVDataset(data_file_path=val_file_path, class_list=class_file_path,
+                                     transform=transforms.Compose([Normalizer(), Resizer()]))
+        return train_dataset, val_dataset
     raise ValueError('dataset_name `{}` is not expected'.format(dataset_name))
 
 
