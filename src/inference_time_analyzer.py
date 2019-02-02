6453df174c96efdc64c2b7ce89599b209ffb9330
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-02-01 22:46:34 -0800

    fixed a typo

--- src/inference_time_analyzer.py
+++ src/inference_time_analyzer.py
@@ -79,7 +79,7 @@ def plot_inference_time(results):
     module_name_list = list()
     mean_exec_time_list = list()
     std_exec_time_list = list()
-    print('Module name\tExecution time[sec]')
+    print('Module name\tExecution time [sec]')
     for i, (module_name, exec_times) in enumerate(results[1:]):
         module_name_list.append(module_name + ': {}'.format(i + 1))
         mean_exec_time_list.append(exec_times.mean())
