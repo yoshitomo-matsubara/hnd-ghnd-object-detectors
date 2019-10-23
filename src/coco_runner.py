07a208ec863ab7af8ee8d41fb4e8c6e1a25d3e8d
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-10-23 15:12:28 -0700

    refactored

--- src/coco_runner.py
+++ src/coco_runner.py
@@ -1,4 +1,4 @@
-r'''PyTorch Detection Training.
+r"""PyTorch Detection Training.
 
 To run in a multi-gpu environment, use the distributed launcher::
 
@@ -8,7 +8,9 @@ To run in a multi-gpu environment, use the distributed launcher::
 The default hyperparameters are tuned for training on 8 gpus and 2 images per gpu.
     --lr 0.02 --batch-size 2 --world-size 8
 If you use different number of gpus, the learning rate should be changed to 0.02/8*$NGPU.
-'''
+"""
+import argparse
+import builtins as __builtin__
 import datetime
 import math
 import os
@@ -22,13 +24,97 @@ import torchvision.models.detection
 import torchvision.models.detection.mask_rcnn
 import torchvision.models.detection.mask_rcnn
 
-from utils import misc_util
 from myutils.common import file_util
 from structure.sampler import GroupedBatchSampler, create_aspect_ratio_groups
 from structure.transformer import ToTensor, RandomHorizontalFlip, Compose
+from utils import misc_util
 from utils.coco_eval_util import CocoEvaluator
 from utils.coco_util import get_coco, get_coco_kp
 from utils.coco_util import get_coco_api_from_dataset
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def init_distributed_mode(args):
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ['RANK'])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+    else:
+        print('Not using distributed mode')
+        args.distributed = False
+        return
+
+    args.distributed = True
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = 'nccl'
+    print('| distributed init (rank {}): {}'.format(args.rank, args.dist_url), flush=True)
+    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                         world_size=args.world_size, rank=args.rank)
+    torch.distributed.barrier()
+    setup_for_distributed(args.rank == 0)
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--data-path', default='/datasets01/COCO/022719/', help='dataset')
+    parser.add_argument('--dataset', default='coco', help='dataset')
+    parser.add_argument('--model', default='maskrcnn_resnet50_fpn', help='model')
+    parser.add_argument('--device', default='cuda', help='device')
+    parser.add_argument('-b', '--batch-size', default=2, type=int,
+                        help='images per gpu, the total batch size is $NGPU x batch_size')
+    parser.add_argument('--epochs', default=13, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--lr', default=0.02, type=float,
+                        help='initial learning rate, 0.02 is the default value for training '
+                             'on 8 gpus and 2 images_per_gpu')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('--lr-step-size', default=8, type=int, help='decrease lr every step-size epochs')
+    parser.add_argument('--lr-steps', default=[8, 11], nargs='+', type=int, help='decrease lr every step-size epochs')
+    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
+    parser.add_argument('--print-freq', default=20, type=int, help='print frequency')
+    parser.add_argument('--output-dir', default='.', help='path where to save')
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--aspect-ratio-group-factor', default=0, type=int)
+    parser.add_argument(
+        '--test-only',
+        dest='test_only',
+        help='Only test the model',
+        action='store_true',
+    )
+    parser.add_argument(
+        '--pretrained',
+        dest='pretrained',
+        help='Use pre-trained models from the modelzoo',
+        action='store_true',
+    )
+
+    # distributed training parameters
+    parser.add_argument('--world-size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
+    return parser
 
 
 def get_dataset(name, image_set, transform, data_path):
@@ -37,7 +123,6 @@ def get_dataset(name, image_set, transform, data_path):
         'coco_kp': (data_path, get_coco_kp, 2)
     }
     p, ds_fn, num_classes = paths[name]
-
     ds = ds_fn(p, image_set=image_set, transforms=transform)
     return ds, num_classes
 
@@ -55,26 +140,21 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     metric_logger = misc_util.MetricLogger(delimiter='  ')
     metric_logger.add_meter('lr', misc_util.SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Epoch: [{}]'.format(epoch)
-
     lr_scheduler = None
     if epoch == 0:
         warmup_factor = 1. / 1000
         warmup_iters = min(1000, len(data_loader) - 1)
-
         lr_scheduler = misc_util.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
 
     for images, targets in metric_logger.log_every(data_loader, print_freq, header):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
         loss_dict = model(images, targets)
-
         losses = sum(loss for loss in loss_dict.values())
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = misc_util.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
         loss_value = losses_reduced.item()
 
         if not math.isfinite(loss_value):
@@ -85,7 +165,6 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         optimizer.zero_grad()
         losses.backward()
         optimizer.step()
-
         if lr_scheduler is not None:
             lr_scheduler.step()
 
@@ -149,7 +228,10 @@ def evaluate(model, data_loader, device):
 
 
 def main(args):
-    misc_util.init_distributed_mode(args)
+    if args.output_dir:
+        file_util.make_dirs(args.output_dir)
+
+    init_distributed_mode(args)
     print(args)
 
     device = torch.device(args.device)
@@ -198,9 +280,7 @@ def main(args):
     optimizer = torch.optim.SGD(
         params, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.lr_steps, gamma=args.lr_gamma)
-
     if args.resume:
         checkpoint = torch.load(args.resume, map_location='cpu')
         model_without_ddp.load_state_dict(checkpoint['model'])
@@ -235,56 +315,5 @@ def main(args):
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(
-        description=__doc__)
-
-    parser.add_argument('--data-path', default='/datasets01/COCO/022719/', help='dataset')
-    parser.add_argument('--dataset', default='coco', help='dataset')
-    parser.add_argument('--model', default='maskrcnn_resnet50_fpn', help='model')
-    parser.add_argument('--device', default='cuda', help='device')
-    parser.add_argument('-b', '--batch-size', default=2, type=int,
-                        help='images per gpu, the total batch size is $NGPU x batch_size')
-    parser.add_argument('--epochs', default=13, type=int, metavar='N',
-                        help='number of total epochs to run')
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-    parser.add_argument('--lr', default=0.02, type=float,
-                        help='initial learning rate, 0.02 is the default value for training '
-                        'on 8 gpus and 2 images_per_gpu')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
-    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)',
-                        dest='weight_decay')
-    parser.add_argument('--lr-step-size', default=8, type=int, help='decrease lr every step-size epochs')
-    parser.add_argument('--lr-steps', default=[8, 11], nargs='+', type=int, help='decrease lr every step-size epochs')
-    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
-    parser.add_argument('--print-freq', default=20, type=int, help='print frequency')
-    parser.add_argument('--output-dir', default='.', help='path where to save')
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--aspect-ratio-group-factor', default=0, type=int)
-    parser.add_argument(
-        '--test-only',
-        dest='test_only',
-        help='Only test the model',
-        action='store_true',
-    )
-    parser.add_argument(
-        '--pretrained',
-        dest='pretrained',
-        help='Use pre-trained models from the modelzoo',
-        action='store_true',
-    )
-
-    # distributed training parameters
-    parser.add_argument('--world-size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
-
-    args = parser.parse_args()
-
-    if args.output_dir:
-        file_util.make_dirs(args.output_dir)
-
-    main(args)
+    parser = get_argparser()
+    main(parser.parse_args())
