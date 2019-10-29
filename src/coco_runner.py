1cfac5026a775bbc5a735e8e2094446fdd4bf778
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-10-29 14:23:01 -0700

    refactored

--- src/coco_runner.py
+++ src/coco_runner.py
@@ -10,10 +10,8 @@ The default hyperparameters are tuned for training on 8 gpus and 2 images per gp
 If you use different number of gpus, the learning rate should be changed to 0.02/8*$NGPU.
 """
 import argparse
-import builtins as __builtin__
 import datetime
 import math
-import os
 import sys
 import time
 
@@ -23,45 +21,9 @@ from torch import nn
 
 from myutils.common import file_util, yaml_util
 from myutils.pytorch import func_util
-from utils import data_util, misc_util, model_util
+from utils import data_util, main_util, misc_util, model_util
 from utils.coco_eval_util import CocoEvaluator
 from utils.coco_util import get_coco_api_from_dataset
-
-
-def setup_for_distributed(is_master):
-    """
-    This function disables printing when not in master process
-    """
-    builtin_print = __builtin__.print
-
-    def print(*args, **kwargs):
-        force = kwargs.pop('force', False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
-
-    __builtin__.print = print
-
-
-def init_distributed_mode(args):
-    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
-        args.rank = int(os.environ['RANK'])
-        args.world_size = int(os.environ['WORLD_SIZE'])
-        args.gpu = int(os.environ['LOCAL_RANK'])
-    elif 'SLURM_PROCID' in os.environ:
-        args.rank = int(os.environ['SLURM_PROCID'])
-        args.gpu = args.rank % torch.cuda.device_count()
-    else:
-        print('Not using distributed mode')
-        return False
-
-    torch.cuda.set_device(args.gpu)
-    args.dist_backend = 'nccl'
-    print('| distributed init (rank {}): {}'.format(args.rank, args.dist_url), flush=True)
-    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                         world_size=args.world_size, rank=args.rank)
-    torch.distributed.barrier()
-    setup_for_distributed(args.rank == 0)
-    return True
 
 
 def get_argparser():
@@ -184,7 +146,7 @@ def train(model, train_sampler, train_data_loader, val_data_loader, device, dist
 
 
 def main(args):
-    distributed = init_distributed_mode(args)
+    distributed, device_ids = main_util.init_distributed_mode(args.world_size, args.dist_url)
     config = yaml_util.load_yaml_file(args.config)
     device = torch.device(args.device)
     print(args)
@@ -198,7 +160,7 @@ def main(args):
     model_config = config['model']
     model = model_util.get_model(model_config, device)
     if distributed:
-        model = nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model = nn.parallel.DistributedDataParallel(model, device_ids=device_ids)
 
     if args.train:
         print('Start training')
