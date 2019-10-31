03128aa70681396af74bf81cc29e282abe8bf715
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-10-30 17:39:29 -0700

    fixed getter bugs

--- src/distillation/tool.py
+++ src/distillation/tool.py
@@ -1,6 +1,7 @@
 from torch import nn
 
 from distillation.loss import get_loss
+from myutils.pytorch import module_util
 
 
 class DistillationBox(nn.Module):
@@ -19,8 +20,8 @@ class DistillationBox(nn.Module):
 
         for loss_name, loss_config in criterion_config['terms'].items():
             teacher_path, student_path = loss_config['ts_modules']
-            teacher_module = getattr(self.teacher_model, teacher_path)
-            student_module = getattr(self.student_model, student_path)
+            teacher_module = module_util.get_module(self.teacher_model, teacher_path)
+            student_module = module_util.get_module(self.student_model, student_path)
             teacher_module.__dict__['loss_name'] = loss_name
             student_module.__dict__['loss_name'] = loss_name
             teacher_module.__dict__['path_from_root'] = teacher_path
