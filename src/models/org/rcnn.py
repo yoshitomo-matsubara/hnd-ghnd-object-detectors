45b76fa2880fa63f2b3bebefd8b9b319c5c276f2
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-11-20 16:34:05 -0800

    fixed a typo

--- src/models/org/rcnn.py
+++ src/models/org/rcnn.py
@@ -101,7 +101,7 @@ class CustomRCNN(nn.Module):
                 ext_cls_loss = nn.functional.binary_cross_entropy_with_logits(ext_logits, ext_targets)
                 loss_dict.update({'ext_cls_loss': ext_cls_loss})
             elif features is None and ext_logits is None:
-                return [{'boxex': torch.empty(0, 4), 'labels': torch.empty(0, dtype=torch.int64),
+                return [{'boxes': torch.empty(0, 4), 'labels': torch.empty(0, dtype=torch.int64),
                          'scores': torch.empty(0)}]
 
         if isinstance(features, torch.Tensor):
