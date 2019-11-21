9f13ed308eb523eaaab05365b45dff1cbec28b3a
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-11-21 11:01:25 -0800

    forgot to add split name

--- src/ext_runner.py
+++ src/ext_runner.py
@@ -155,7 +155,7 @@ def main(args):
         total_time = time.time() - start_time
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
         print('Training time {}'.format(total_time_str))
-    evaluate(model, test_data_loader, device=device)
+    evaluate(model, test_data_loader, device=device, split_name='Test')
 
 
 if __name__ == '__main__':
