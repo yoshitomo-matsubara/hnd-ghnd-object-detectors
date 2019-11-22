0068b373251b4e377368d27d5c2f12d664fb8a94
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-11-21 19:42:24 -0800

    fixed a typo

--- src/models/ext/classifier.py
+++ src/models/ext/classifier.py
@@ -15,7 +15,7 @@ class BaseExtClassifier(nn.Module):
 
 class Ext4ResNet(BaseExtClassifier):
     def __init__(self, resnet):
-        super().__init__(ext_idx=1)
+        super().__init__(ext_idx=0)
         # inplanes = 512 for ResNets-18 and -34, and 2048 for all the other ResNets
         input_channel = resnet.inplanes // 8
         self.extractor = nn.Sequential(
