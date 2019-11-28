e6401e7fcce715e00737e771035448d25aa87f1c
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-11-27 17:08:31 -0800

    updated default parameters, and added mimic layer for smaller resnet

--- src/models/mimic/resnet_layer.py
+++ src/models/mimic/resnet_layer.py
@@ -1,7 +1,40 @@
 from torch import nn
 
 
-class Bottleneck4ResNet50(nn.Module):
+class Bottleneck4SmallResNet(nn.Module):
+    def __init__(self, bottleneck_channel):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 256, kernel_size=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 64, kernel_size=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, bottleneck_channel, kernel_size=2, padding=1, bias=False)
+        )
+        self.decoder = nn.Sequential(
+            nn.BatchNorm2d(bottleneck_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(bottleneck_channel, 64, kernel_size=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, kernel_size=2, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
+
+
+class Bottleneck4LargeResNet(nn.Module):
     def __init__(self, bottleneck_channel):
         super().__init__()
         self.encoder = nn.Sequential(
@@ -39,7 +72,10 @@ def get_mimic_layers(backbone_name, backbone_params_config):
     layer1_config = backbone_params_config.get('layer1', None)
     if layer1_config is not None:
         layer1_name = layer1_config['name']
-        if layer1_name == 'Bottleneck4ResNet50' and backbone_name == 'custom_resnet50':
-            layer1 = Bottleneck4ResNet50(layer1_config['bottleneck_channel'])
+        if layer1_name == 'Bottleneck4SmallResNet' and backbone_name in {'custom_resnet18', 'custom_resnet34'}:
+            layer1 = Bottleneck4LargeResNet(layer1_config['bottleneck_channel'])
+        elif layer1_name == 'Bottleneck4LargeResNet'\
+                and backbone_name in {'custom_resnet50', 'custom_resnet101', 'custom_resnet152'}:
+            layer1 = Bottleneck4LargeResNet(layer1_config['bottleneck_channel'])
     return layer1, layer2, layer3, layer4
 
