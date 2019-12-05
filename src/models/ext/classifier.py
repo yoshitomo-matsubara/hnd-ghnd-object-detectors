10a9c586d8aa0ac96ac21385ef350a23972ec4a7
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-12-05 12:10:32 -0800

    made ext classifier deeper

--- src/models/ext/classifier.py
+++ src/models/ext/classifier.py
@@ -20,12 +20,16 @@ class Ext4ResNet(BaseExtClassifier):
             nn.AdaptiveAvgPool2d((64, 64)),
             nn.Conv2d(input_channel, 64, kernel_size=4, stride=2),
             nn.BatchNorm2d(64),
-            nn.Conv2d(64, 32, kernel_size=4, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 32, kernel_size=3, stride=2),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool2d((4, 4))
+            nn.Conv2d(32, 16, kernel_size=2, stride=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((8, 8))
         )
-        self.linear = nn.Linear(32 * 4 * 4, 2)
+        self.linear = nn.Linear(16 * 8 * 8, 2)
 
     def forward(self, x):
         z = self.extractor(x)
