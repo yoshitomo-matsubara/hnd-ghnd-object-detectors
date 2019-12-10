76a2ec10c73ef021cbf184f7c6595b3df5c6dd3e
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2019-12-10 11:04:49 -0800

    forgot to add one line

--- src/cost_analyzer.py
+++ src/cost_analyzer.py
@@ -230,6 +230,7 @@ def analyze_split_model_inference(model, device, quantization, head_only, datase
         tail_proc_time_list.append(tail_proc_time)
         total_proc_time_list.append(head_proc_time + tail_proc_time)
 
+    coco_evaluator.synchronize_between_processes()
     coco_evaluator.accumulate()
     coco_evaluator.summarize()
     summarize_inference_time(head_proc_time_list, tail_proc_time_list, total_proc_time_list)
