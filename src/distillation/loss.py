65b1a3d4fabe789e45ecc0c39ec592e7f05777ae
Yoshitomo Matsubara <yoshitom@uci.edu>
2020-07-12 13:13:55 -0700

    deleted non-tested module

--- src/distillation/loss.py
+++ src/distillation/loss.py
@@ -34,41 +34,8 @@ class GeneralizedCustomLoss(CustomLoss):
         return sub_total_loss + self.org_loss_factor * sum(loss for loss in org_loss_dict.values())
 
 
-class HKDLoss4RCNN(CustomLoss):
-    """
-    Hierarchical Knowledge Distillation loss (when org_loss_factor = 0.0) defined in
-        ``Learning Lightweight Pedestrian Detector with Hierarchical Knowledge Distillation''
-        https://ieeexplore.ieee.org/document/8803079
-    """
-
-    def __init__(self, criterion_config):
-        super().__init__(criterion_config)
-
-    @staticmethod
-    def loss_as_dict(teacher_output, student_output, criterion, mean=True):
-        sum_loss = sum(criterion(student_output[key], teacher_output[key]) for key in teacher_output.keys())
-        return sum_loss / len(teacher_output.keys()) if mean else sum_loss
-
-    def forward(self, output_dict, org_loss_dict):
-        kd_loss_dict = dict()
-        for loss_name, ((teacher_path, teacher_output), (student_path, student_output)) in output_dict.items():
-            _, criterion, factor = self.term_dict[loss_name]
-            if teacher_path == 'backbone.fpn':
-                kd_loss_dict[loss_name] = self.loss_as_dict(teacher_output, student_output, criterion) * factor
-            elif teacher_path == 'roi_heads.box_predictor':
-                # TODO: check shape of each output to see #proposals
-                kd_loss_dict[loss_name] =\
-                    sum(criterion(to_i, so_i) for to_i, so_i in zip(teacher_output, student_output)) / 2 * factor
-            else:
-                kd_loss_dict[loss_name] = criterion(teacher_output, student_output) * factor
-
-        org_loss = sum(loss for loss in org_loss_dict.values())
-        return org_loss * self.org_loss_factor + sum(loss for loss in kd_loss_dict.values())
-
-
 LOSS_DICT = {
-    'general': GeneralizedCustomLoss,
-    'hkd4rcnn': HKDLoss4RCNN
+    'general': GeneralizedCustomLoss
 }
 
 
